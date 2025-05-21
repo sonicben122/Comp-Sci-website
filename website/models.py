2c80021b98790da4db955eb944990748c93da01d
Ben <benparoulek@gmail.com>
2025-05-21 13:23:01 -0400

    Added Game log

--- website/models.py
+++ website/models.py
@@ -7,6 +7,8 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    kda = db.Column(db.String(100))
+    gamedate = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
