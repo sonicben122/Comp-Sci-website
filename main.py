85e41d626e268b2cae503bd83fd45e0adbc0b10b
sonicben122 <benparoulek@gmail.com>
2025-06-07 23:54:01 -0400

    updated requirerments

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from website import create_app
-from csnews import (
-    latestupdatetitle, update2title, update3title, update4title,
-    latestupdateurl, update2url, update3url, update4url
-)
 
 app = create_app()
 
